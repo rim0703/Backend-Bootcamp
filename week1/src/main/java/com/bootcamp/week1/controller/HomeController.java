1b86960b000e31c5b4faf1d8b6faeb94df153de0
rim0703 <rim0703@yonsei.ac.kr>
2022-08-10 17:04:18 +0900

    fix: Error 500, 404 처리완료

--- week1/src/main/java/com/bootcamp/week1/controller/HomeController.java
+++ week1/src/main/java/com/bootcamp/week1/controller/HomeController.java
@@ -1,34 +1,39 @@
 package com.bootcamp.week1.controller;
 
-import javax.inject.Inject;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.bootcamp.week1.service.EntityService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-
-	@Inject
-	EntityService entityService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String home() {
-	    	logger.info("requesting home");
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
 		return "home";
 	}
-
+	
 }
-
